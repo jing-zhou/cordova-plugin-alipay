505c2b4647a431c583ea83c4d809b5fc9c1e9d03
jingzhou <jingzhou_wang@hotmail.com>
2018-07-05 11:18:30 +0800

    Alipay.TAG = "org.apache.cordova.ali.Alipay";

--- src/android/Alipay.java
+++ src/android/Alipay.java
@@ -23,7 +23,7 @@ public class Alipay extends CordovaPlugin {
     private static final String RESULT_STATUS = "resultStatus";
     private static final String RESULT = "result";
     private static final String MEMO = "memo";
-    private static final String TAG = "cordova-alipay";
+    private static final String TAG = "org.apache.cordova.ali.Alipay";
     private static final String Execute = "Execute: ";
     private static final String with = " with: ";
     private static final String unsupported_param = "Unsported parameter:";
